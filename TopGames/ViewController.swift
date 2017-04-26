72d087c7e8a8fa24f5c6b352c9def8b66813d3ca
Alina Hambaryan <ah@alinas-macbook-pro.local>
2017-04-26 14:05:53 +0200

    Initial Commit

---
+++ TopGames/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TopGames
+//
+//  Created by ALINA HAMBARYAN on 4/26/17.
+//  Copyright © 2017 ALINA HAMBARYAN. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
